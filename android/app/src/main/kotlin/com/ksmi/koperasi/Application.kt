5bdfbca944f934c1920239db4c3f0043b82b5701
fahrulriadu <fahrul@fahrul-linux.bbrouter>
2025-11-10 10:38:56 -0500

    Upgrade compileSdk & ndkVersion to fix debug build FINAL 18

--- android/app/src/main/kotlin/com/ksmi/koperasi/Application.kt
+++ android/app/src/main/kotlin/com/ksmi/koperasi/Application.kt
@@ -2,19 +2,18 @@ package com.ksmi.koperasi
 
 import io.flutter.app.FlutterApplication
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugins.GeneratedPluginRegistrant
-import be.tramckrijte.workmanager.WorkmanagerPlugin
 
 class Application : FlutterApplication() {
     override fun onCreate() {
         super.onCreate()
+        // Tidak perlu register plugin manual lagi
+        // WorkManager otomatis diinisialisasi
     }
 
     companion object {
         fun registerWith(flutterEngine: FlutterEngine) {
             GeneratedPluginRegistrant.registerWith(flutterEngine)
-            WorkmanagerPlugin.registerWith(flutterEngine)
         }
     }
 }
